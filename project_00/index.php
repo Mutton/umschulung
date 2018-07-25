689ff93ef27e882973b07e826f0dd884381ca0a6
Florian R. Klein <torpedoente@googlemail.com>
2018-07-25 22:14:20 +0200

    playing with args

--- project_00/index.php
+++ project_00/index.php
@@ -1,11 +1,31 @@
 <?php
 declare(strict_types=1);
 
-function someFunc(string $str) : string {
+function echoNewLine (bool $leadingNewLine, string ...$strings) {
+  $newLine = "<br>";
+  $firstLine = true;
+  foreach ($strings as $string) {
+    if ($firstLine && (!$leadingNewLine))
+    {
+      $firstLine = false;
+      echo $string;
+    }
+    else
+    {
+      echo $newLine.$string;
+    }
+  }
+}
+
+function someFunc (string $str) : string {
   return $str." GOTCHA :D ";
 }
 
-// echo someFunc("333");
-// echo "<br>";
-echo date("l, Y-m-d h:i:s a")
+$shellCommand = "dir";
+$shellResult = shell_exec($shellCommand);
+
+echoNewLine(false,
+  date("l, Y-m-d h:i:s a"),
+  $shellResult
+);
 ?>
